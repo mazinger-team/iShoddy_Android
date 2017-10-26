29b14a2840fcd803a0a393be851409447b2ea782
josejacin <joseynene@hotmail.com>
2017-10-26 03:58:00 +0200

    Finalizado el desarrollo Falta por terminar. Que se muestre la actividad cuando se pulsa en todos Que se muestre el detalle del profesional Pasarlo todo a Fragments

--- iShoddy/app/src/main/java/com/mazinger/ishoddy/activities/ProfessionalsListActivity.java
+++ iShoddy/app/src/main/java/com/mazinger/ishoddy/activities/ProfessionalsListActivity.java
@@ -1,8 +1,17 @@
 package com.mazinger.ishoddy.activities;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
@@ -33,35 +42,28 @@ import com.mazinger.ishoddy.util.EndlessScrollListener;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ProfessionalsListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, SearchView.OnQueryTextListener
-{
+public class ProfessionalsListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, SearchView.OnQueryTextListener {
     @BindView(R.id.toolbar) Toolbar mToolbar;
     @BindView(R.id.activity_list_professionals__relativeLayout_linearLayout_progress_bar) ProgressBar mProgressBar;
 
-    //RecyclerView mRecyclerView;
     GridView mGridView;
 
-    //ProfessionalRecyclerViewAdapter mRecyclerViewAdapter;
     ProfessionalListViewAdapter mProfessionalListViewAdapter = null;
-    //RecyclerView.LayoutManager mLayoutManager;
     Professionals mProfessionals = new Professionals();
 
     String filter = null;
     String order = null;
     String fields = null;
 
-    Double gps_lat = 0.0;
-    Double gps_lon  = 0.0;
-
-    Integer page = 0;
+    public Double gps_lat = 0.0;
+    public Double gps_lon  = 0.0;
 
     Boolean loadingStatus = false;
 
     Pagination mPagination = new Pagination(false).setPaginationKey(0).setPaginationElements(10);
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_professionals);
 
@@ -69,11 +71,6 @@ public class ProfessionalsListActivity extends AppCompatActivity implements Adap
         setSupportActionBar(mToolbar);
 
         obtainProfessionalsList(filter, order, fields, mPagination.getPaginationKey());
-
-        mGridView = (GridView)findViewById(R.id.activity_list_professionals__relativeLayout_linearLayout_coordinatorLayout_gridView);
-        mProfessionalListViewAdapter = new ProfessionalListViewAdapter(getApplicationContext(), mProfessionals);
-        mGridView.setAdapter(mProfessionalListViewAdapter);
-
     }
 
     private void obtainProfessionalsList(final String filter, final String order, final String fields, Integer page) {
@@ -82,94 +79,121 @@ public class ProfessionalsListActivity extends AppCompatActivity implements Adap
 
             mProgressBar.setVisibility(View.VISIBLE);
 
-        /*
+            obtainPosition();
 
-        // If location services is enabled get the users location
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager = CLLocationManager()
-            locationManager?.requestWhenInUseAuthorization()
-            locationManager?.delegate = self
-            locationManager?.desiredAccuracy = kCLLocationAccuracyBest // You can change the locaiton accuary here.
-            locationManager?.startUpdatingLocation()
-            locationManager?.requestLocation()
-            if let location = locationManager?.location?.coordinate {
-                gps_lat = location.latitude
-                gps_lon = location.longitude
-            }
-        }
+            GetAllProfessionalManager manager = new GetAllAllProfessionalsManagerImpl(this);
+            GetAllProfessionalsInteractor getAllProfessionalsInteractor = new GetAllProfessionalsInteractorImp(manager);
+            getAllProfessionalsInteractor.execute(
+                    filter, order, fields, page, new GetAllProfessionalsInteractorCompletion() {
+                        @Override
+                        public void completion(Professionals professionals, Pagination pagination) {
+                            mProfessionals.addAll(professionals);
+                            mPagination = pagination;
+                            mProgressBar.setVisibility(View.INVISIBLE);
 
-        automaticallyAdjustsScrollViewInsets = false
+                            initializeViews();
 
-        if (gps_lat != 0 && gps_lon != 0) {
-            filter = "gps_lat=" + String(describing: gps_lat!)
-            filter = filter! + "&gps_lon=" + String(describing: gps_lon!)
-        }
-
-
-         */
-        GetAllProfessionalManager manager = new GetAllAllProfessionalsManagerImpl(this);
-        GetAllProfessionalsInteractor getAllProfessionalsInteractor = new GetAllProfessionalsInteractorImp(manager);
-        getAllProfessionalsInteractor.execute(
-                filter, order, fields, page, new GetAllProfessionalsInteractorCompletion() {
-                    @Override
-                    public void completion(Professionals professionals, Pagination pagination)
-                    {
-                        //setupRecyclerView(professionals);
-                        mProfessionals.addAll(professionals);
-                        mPagination = pagination;
-                        mProgressBar.setVisibility(View.INVISIBLE);
-
-
-
-                        mGridView.setOnScrollListener(new EndlessScrollListener() {
-                            @Override
-                            public boolean onLoadMore(int page, int totalItemsCount) {
-                                if (mPagination.getPaginationFlag()) {
-                                    loadingStatus = false;
-                                    if (mPagination.getPaginationKey() == 0) {
-                                        mPagination.setPaginationKey(2);
-                                    } else {
-                                        mPagination.setPaginationKey(mPagination.getPaginationKey()+1);
-                                    }
-
-                                    obtainProfessionalsList(filter, order, fields, mPagination.getPaginationKey());
-                                    return true;
-                                } else {
-                                    return false;
-                                }
-                            }
-                        });
+                            onScrollListener(filter, order, fields);
+                        }
+                    },
+                    new InteractorErrorCompletion() {
+                        @Override
+                        public void onError(String errorDescription) {
+                            mProgressBar.setVisibility(View.INVISIBLE);
+                        }
                     }
-                },
-                new InteractorErrorCompletion()
-                {
-                    @Override
-                    public void onError(String errorDescription)
-                    {
-                        mProgressBar.setVisibility(View.INVISIBLE);
-                    }
-                }
-        );
-
-        /*
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager?.stopUpdatingLocation()
-        }
-         */
+            );
         }
     }
 
-    private void setupRecyclerView(Professionals professionals)
-    {
-        //mRecyclerViewAdapter = new ProfessionalRecyclerViewAdapter(getBaseContext(), professionals);
-        //mRecyclerView.setAdapter(mRecyclerViewAdapter);
+    private void obtainPosition() {
+        // Se solicitan los permisos al usuario
 
-        mProfessionals = professionals;
+        if (ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            //showAlert();
+            return;
+        }
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        activeLocation(locationManager);
+        Criteria criteria = new Criteria();
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+        if (location != null) {
+            gps_lat = location.getLatitude();
+            gps_lon = location.getLongitude();
+            if (gps_lat != 0 && gps_lon != 0) {
+                filter = "gps_lat=" + gps_lat;
+                filter = filter + "&gps_lon=" + gps_lon;
+            }
+        }
+    }
+
+    private void activeLocation(LocationManager mlocManager) {
+        final boolean gpsEnabled = mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!gpsEnabled) {
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+        }
+    }
+
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Enable Location")
+                .setMessage("Su ubicación esta desactivada.\npor favor active su ubicación " + "usa esta app")
+                .setPositiveButton("Configuración de ubicación", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(settingsIntent);
+                        obtainProfessionalsList(filter, order, fields, mPagination.getPaginationKey());
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    }
+                });
+        dialog.show();
+    }
+
+    private void onScrollListener(final String filter, final String order, final String fields) {
+        mGridView.setOnScrollListener(new EndlessScrollListener() {
+            @Override
+            public boolean onLoadMore(int page, int totalItemsCount) {
+                if (mPagination.getPaginationFlag()) {
+                    loadingStatus = false;
+                    if (mPagination.getPaginationKey() == 0) {
+                        mPagination.setPaginationKey(2);
+                    } else {
+                        mPagination.setPaginationKey(mPagination.getPaginationKey()+1);
+                    }
+
+                    obtainProfessionalsList(filter, order, fields, mPagination.getPaginationKey());
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+    }
+
+    private void initializeViews() {
+        mGridView = (GridView)findViewById(R.id.activity_list_professionals__relativeLayout_linearLayout_coordinatorLayout_gridView);
+        mProfessionalListViewAdapter = new ProfessionalListViewAdapter(getApplicationContext(), mProfessionals);
+        mGridView.setAdapter(mProfessionalListViewAdapter);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_items, menu);
 
         MenuItem menuItem = menu.findItem(R.id.action_search);
@@ -186,23 +210,18 @@ public class ProfessionalsListActivity extends AppCompatActivity implements Adap
     }
 
     @Override
-    public boolean onQueryTextChange(String newText)
-    {
+    public boolean onQueryTextChange(String newText) {
         newText = newText.toLowerCase();
         Professionals newList = new Professionals();
 
-        for (int i = 0; i < mProfessionals.size(); i++)
-        {
+        for (int i = 0; i < mProfessionals.size(); i++) {
             Professional professional = mProfessionals.get(i);
             String name = professional.getCorp_name().toLowerCase();
 
-            if (name.contains(newText))
-            {
+            if (name.contains(newText)) {
                 newList.add(professional);
             }
         }
-
-        //mRecyclerViewAdapter.setFilter(newList);
 
         return true;
     }
@@ -227,26 +246,6 @@ public class ProfessionalsListActivity extends AppCompatActivity implements Adap
         } else
             startActivity(intent);
     }
-
-      /*
-    @NonNull
-    private InfiniteScrollListener createInfiniteScrollListener() {
-        return new InfiniteScrollListener(MAX_ITEMS_PER_REQUEST, layoutManager) {
-            @Override public void onScrolledToEnd(final int firstVisibleItemPosition) {
-                simulateLoading();
-                int start = ++page * MAX_ITEMS_PER_REQUEST;
-                final boolean allItemsLoaded = start >= items.size();
-                if (allItemsLoaded) {
-                    mProgressBar.setVisibility(View.GONE);
-                } else {
-                    int end = start + MAX_ITEMS_PER_REQUEST;
-                    final List<String> itemsLocal = getItemsToBeLoaded(start, end);
-                    refreshView(recyclerView, new MyAdapter(itemsLocal), firstVisibleItemPosition);
-                }
-            }
-        };
-    }
-*/
 }
 
 
